ec9d15957a5713da2164dfc18c6b93ab944f77a9
emily baier <emilyrbaier@gmail.com>
2019-04-08 13:37:55 -0700

    updated to 4 spaces, working on filter

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
-const removeY = require('../lib/index');
+const { removeY } = require('../lib/index');
 
 describe('array test', ()=> {
-  it('takes an array removes words that contain y', ()=> {
-    const array = ['journal', 'coffee', 'Emily', 'lovely', 'computer'];
-    const result = removeY(array);
-    expect(result).toEqual(['journal', 'coffee', 'computer']);
-  });
+    it('takes an array removes words that contain y', ()=> {
+        const strings = ['journal', 'coffee', 'Emily', 'lovely', 'computer'];
+        const result = removeY(strings);
+        expect(result).toEqual(['journal', 'coffee', 'computer']);
+    });
 });
