126c4163a6df1c335c829653d086303f37a4c731
emily baier <emilyrbaier@gmail.com>
2019-04-11 10:25:15 -0700

    made it pass for lowercase y

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
-const { removeY } = require('../lib/index');
+const { removeLetter } = require('../lib/index');
 
 describe('array test', ()=> {
     it('takes an array removes words that contain y', ()=> {
         const strings = ['journal', 'coffee', 'Emily', 'lovely', 'computer'];
-        const result = removeY(strings);
-        expect(result).toEqual(['journal', 'coffee', 'computer']);
+        const result = removeLetter(strings, 'y');
+        expect(result).toEqual(['Is it journal?', 'Is it coffee?', 'Is it computer?']);
     });
 });
